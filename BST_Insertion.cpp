3e86ecdbae76006b52f342f87323d425061740f5
P-compete <singhsomil3310@gmail.com>
2021-10-05 21:20:27 +0530

    Initial Commit

---
+++ BST_Insertion.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node{
+int data;
+node *left,*right;};
+
+node *root=NULL;
+
+void insert(int a)
+{
+
+}
+
+int main()
+{
+
+    int n;
+     cin>>n;
+     
+     for(int i=0;i<n;i++)
+      {
+         int a;
+          cin>>a;
+          insert(a);
+      }
+     
+  return 0;    
+  
+}
